423ff074c5de60d8505dde227aaa322a455cd417
Sk Zaffar Ekbal <s.zaffarekbal@gmail.com>
2024-02-13 23:06:24 +0530

    Add Placeholder when drag.

--- src/Components/StudentTable.jsx
+++ src/Components/StudentTable.jsx
@@ -48,6 +48,14 @@ function StudentTable() {
     console.log('Rank', index);
   };
 
+  const PlaceHolder = () => (
+    <tr>
+      <td className={tdClass + ' bg-blue-50'} colSpan={6}>
+        {/* &nbsp; */}Drop Here
+      </td>
+    </tr>
+  );
+
   return (
     <div className='max-w-2xl mx-auto mt-8'>
       <h2 className='text-2xl font-bold mb-4'>Student Information(Drag and Drop To Sort)</h2>
@@ -63,24 +71,43 @@ function StudentTable() {
           </tr>
         </thead>
         <tbody>
+          {newRank === 0 && draggedItem?.rank !== 1 ? <PlaceHolder /> : ''}
           {students.map((student, index) => (
-            <tr
-              id={index}
-              className={draggedItem?.rank === student.rank ? 'bg-gray-200' : ''}
-              key={student.rank}
-              draggable
-              onDragStart={(e) => onDragStart(e, index)}
-              onDragEnd={() => onDragEnd(index)}
-              onDragOver={(e) => onDragOver(e, index)}
-              onDragEnter={() => onDragEnter(index)}
-            >
-              <td className={tdClass}>{student.rank}</td>
-              <td className={tdClass}>{student.id}</td>
-              <td className={tdClass}>{student.name}</td>
-              <td className={tdClass}>{student.grade}</td>
-              <td className={tdClass}>{student.percentage}%</td>
-              <td className={tdClass}>{student.age}</td>
-            </tr>
+            <>
+              {newRank !== 0 &&
+              draggedItem?.rank > newRank + 1 &&
+              newRank === index &&
+              index + 1 !== draggedItem?.rank ? (
+                <PlaceHolder />
+              ) : (
+                ''
+              )}
+              <tr
+                id={index}
+                className={draggedItem?.rank === student.rank ? 'bg-gray-200' : ''}
+                key={student.rank}
+                draggable
+                onDragStart={(e) => onDragStart(e, index)}
+                onDragEnd={() => onDragEnd(index)}
+                onDragOver={(e) => onDragOver(e, index)}
+                onDragEnter={() => onDragEnter(index)}
+              >
+                <td className={tdClass}>{student.rank}</td>
+                <td className={tdClass}>{student.id}</td>
+                <td className={tdClass}>{student.name}</td>
+                <td className={tdClass}>{student.grade}</td>
+                <td className={tdClass}>{student.percentage}%</td>
+                <td className={tdClass}>{student.age}</td>
+              </tr>
+              {newRank !== 0 &&
+              draggedItem?.rank < newRank + 1 &&
+              newRank === index &&
+              index + 1 !== draggedItem?.rank ? (
+                <PlaceHolder />
+              ) : (
+                ''
+              )}
+            </>
           ))}
         </tbody>
       </table>
